3a9d6fa819affa9b007d3db42b45554640ec2e32
shrouti <shrouti.gangopadhyay@gmail.com>
2021-02-25 16:01:20 +0530

    message and port changed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8084;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('message changed'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
